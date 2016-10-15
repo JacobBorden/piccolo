924547fdcc4953cd6f0d19828b28d05f7de7c6d2
Jacob Borden <amenra.beats@gmail.com>
2016-10-15 03:45:19 -0300

    Initial Commit
    Related Work Items: #61, #62, #63

---
+++ Piccolo/Piccolo/app.js
@@ -0,0 +1,20 @@
+﻿var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var port = 8888;
+
+app.get('/', function (request, response) {
+    response.sendFile(__dirname + '/index.html')
+});
+
+
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(port);
